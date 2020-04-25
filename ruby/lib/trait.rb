46a96a77d1a365f419d829093e16684ffaeddc22
dpeccia <42644816+dpeccia@users.noreply.github.com>
2020-04-25 13:00:20 -0300

    Definición de Trait

--- ruby/lib/trait.rb
+++ ruby/lib/trait.rb
@@ -1,33 +1,24 @@
 class Trait
-  attr_accessor :nombre,:metodos,:metodos_repetidos
-  def self.define(iniciadores)
-    @nombre = iniciadores.match(/(?<=name :).+/)
-    nombre_metodos = iniciadores.scan(/(?<=method :).+(?=.do)/)
-    create_method(hash[:method],block)
-  end
-  def create_method(name, &block)
-    self.class.send(:define_method, name, &block)
+  attr_reader :nombre
+
+  def initialize(&bloque)
+    self.instance_eval(&bloque)
   end
 
-  def +(otro_trait)
-    #metodos_repetidos = otro_trait.metodos.detect{ |m| self.metodos.count(m) > 1 }
-    otro_trait.methods.each do |m|
-      if self.methods.include? m
-
-      end
-        self.class.send(:define_method, name, &block) do
-          block = {do |*arg| raise "Metodos Con nombres iguales, fijarse de arreglarlo" end}
-        next
-      end
-      define_method (m) do |*arg|
-        otro_trait.send(m,*arg)
-      end
-    end
-      #return self
+  def self.define(&bloque)
+    nuevoTrait = Trait.new(&bloque)
+    Object.const_set(nuevoTrait.nombre,nuevoTrait)
   end
 
+  def name(simbolo)
+    @nombre = simbolo
+  end
 
+  def method(simbolo, &bloque)
+    define_singleton_method(simbolo, &bloque)
+  end
 end
+
 class Class
   def uses un_trait
     include un_trait
