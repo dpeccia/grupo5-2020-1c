359c170a8d24f6358ebbd44f6e4799bb2d598eef
facuhump2409 <32522610+facuhump2409@users.noreply.github.com>
2020-04-27 19:16:30 -0300

    Subo comando + funcionando
    faltan tests, bbajo de todo dejo casos de uso

--- ruby/lib/trait.rb
+++ ruby/lib/trait.rb
@@ -19,13 +19,19 @@ class Trait
     define_singleton_method(simbolo, &bloque)
   end
   def + otro_trait
-    Trait.new()
-    @error = proc {raise 'Metodos duplicados'}
-    @otro_trait = otro_trait
+    error = proc {raise 'Metodo Repetido'}
+    otro_trait.singleton_methods(false).each do |m|
+      if self.methods(false).include? m
+        self.define_singleton_method(m, &error) 
+        next
+      end
+      self.define_singleton_method(m, &otro_trait.singleton_method(m))
+    end
+    return self
   end
-  def method_missing(m, *args, &block)
-    @otro_trait.send(m,*args,&block)
-  end
+  #def method_missing(m, *args, &block)
+   # @otro_trait.send(m,*args,&block)
+  #end
 end
 
 class Class
@@ -38,3 +44,30 @@ class Class
   end
 end
 
+
+Trait.define do
+  name :MiTrait
+  method :metodo1 do
+    "Hola"
+  end
+  method :metodo2 do |un_numero|
+    un_numero * 0 + 42
+  end
+end
+
+Trait.define do
+  name :MiOtroTrait
+  method :metodo1 do
+  "kawuabonga" 
+  end
+  method :metodo3 do
+  "zaraza" 
+  end 
+end
+
+class A uses MiTrait + MiOtroTrait 
+end
+
+A.new.metodo1
+A.new.metodo3
+A.new.metodo2 2
